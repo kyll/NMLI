036a28843c91c09f0395de94c8b74664aec47921
Kyllian Broers <kyllianbroers@live.nl>
2014-03-31 11:37:15 +0200

    eerste opzet

--- src/CountNGrams.java
+++ src/CountNGrams.java
@@ -1,8 +1,33 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
 
 public class CountNGrams {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int n;
+		try{
+			n = Integer.parseInt(args[0]);
+			
+			
+		}catch(Exception e){
+			System.out.print(e);
+			System.exit(0);
+		}
+		try {
+            BufferedReader src = new BufferedReader(new FileReader(args[1]));
+            String str = src.readLine();
+            while (str != null) {
+                StringTokenizer st = new StringTokenizer(str, " ,.:;\"-_(){}[]?!*^&'\n\t");
+                while(st.hasMoreTokens()) {
+                    String word = st.nextToken();
+                    //hier moet shit
+                }
+                str = src.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 	}
 
