215189327106e8caf7dce9a203fe5d2cd675bc33
Kyllian Broers <kyllianbroers@live.nl>
2014-03-31 12:53:54 +0200

    eerste ding gedaan

--- src/CountNGrams.java
+++ src/CountNGrams.java
@@ -1,11 +1,15 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 
 public class CountNGrams {
-
+	
+	private static int n;
+	private static String[] sequence;
+	
 	public static void main(String[] args) {
-		int n;
+
 		try{
 			n = Integer.parseInt(args[0]);
 			
@@ -14,6 +18,9 @@ public class CountNGrams {
 			System.out.print(e);
 			System.exit(0);
 		}
+		
+		sequence = new String[n];
+		Arrays.fill(sequence, "");
 		try {
             BufferedReader src = new BufferedReader(new FileReader(args[1]));
             String str = src.readLine();
@@ -21,16 +28,27 @@ public class CountNGrams {
                 StringTokenizer st = new StringTokenizer(str, " ,.:;\"-_(){}[]?!*^&'\n\t");
                 while(st.hasMoreTokens()) {
                     String word = st.nextToken();
-                    if () {
-                        //hier moet shit
+                    if (true) {
+                        put(word);
+                        getSequence();
                     }
                 }
                 str = src.readLine();
             }
+            src.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+	}
+
+	private static void put(String word) {
+		if (sequence.length > 2) {
+			for (int i = 1; i < sequence.length; i++) {
+				sequence[i-1] = sequence[i]; 
+			}
+		}
+		sequence[sequence.length-1] = word;
 	}
 
 }
