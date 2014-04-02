97dcb99889702b6f076b193f086029f15da1854c
Kyllian Broers <kyllianbroers@live.nl>
2014-04-02 11:49:46 +0200

    klaar!

--- src/CountNGrams.java
+++ src/CountNGrams.java
@@ -9,28 +9,31 @@ import java.util.StringTokenizer;
 public class CountNGrams {
 	
 	private static int n;
+	private static int m;
 	private static String[] sequence;
 	private static Hashtable<String, Integer> table = new Hashtable<String, Integer>();
 	private static String[][] array;
 	
 	public static void main(String[] args) {
 
+		System.out.print("Loading");
 		try{
-			n = Integer.parseInt(args[0]);
-			
+			n = Integer.parseInt(args[1]);
+			m = Integer.parseInt(args[2]);
 			
 		}catch(Exception e){
-			System.out.print(e);
-			System.exit(0);
+			e.printStackTrace();
+			System.exit(1);
 		}
 		
 		sequence = new String[n];
 		Arrays.fill(sequence, "");
+		
 		try {
-            BufferedReader src = new BufferedReader(new FileReader(args[1]));
+            BufferedReader src = new BufferedReader(new FileReader(args[0]));
             String str = src.readLine();
             while (str != null) {
-                StringTokenizer st = new StringTokenizer(str, " ,.:;\"-_(){}[]?!*^&'\n\t");
+                StringTokenizer st = new StringTokenizer(str, " ,.:;\"(){}[]?!*^\n\t");
                 while(st.hasMoreTokens()) {
                     String word = st.nextToken();
                        put(word);
@@ -44,27 +47,102 @@ public class CountNGrams {
                 str = src.readLine();
             }
             src.close();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
+            System.exit(1);
         }
+		System.out.print(".");
+		
 		hashToArray();
+		System.out.print(".");
+		
 		sortArray();
-		printArray();
+		System.out.print(".");
+		
+		//writeArray();
+		//System.out.println(".");
+		
+		int sum = sum();
+		System.out.print(".");
+		
+		System.out.println("\nDone!\n");		
+		System.out.println(sum);
+		printArray(0,m);
+	}
+
+	private static int sum() {
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum += Integer.parseInt(array[i][1]);
+		}
+		return sum;
 	}
 
 	private static void printArray() {
-		for (String[] s : array) {
-            System.out.println(s[0] + " " + s[1]);
-        }
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i][0] + "- " + array[i][1]);
+		}
+	}
+	
+	private static void printArray(int from, int to) {
+		if (from > to) {
+			int tmp = from;
+			from = to;
+			to = tmp;
+		}
+		
+		if(from < 0) {
+			from = 0;
+		}else if(from > array.length) {
+			from = array.length;
+		}
+		
+		if (to > array.length) {
+			to = array.length;
+		}else if (to < 0) {
+			to = 0;
+		}
+		
+		if (from == to) {
+			System.out.println(array[from][0] + "- " + array[from][1]);
+			return;
+		}
+
+		for (int i = from; i < to; i++) {
+			System.out.println(array[i][0] + "- " + array[i][1]);
+		}
+	}
+
+	private static void writeArray() {
+		try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(new File(
+                            "Ngram.txt"), true));
+			for (int i = 0; i < array.length; i++) {
+				System.out.println(array[i][0] + " " + array[i][1]);
+            
+				bw.write(array[i][0] + " " + array[i][1]);
+				bw.newLine();
+			}
+			bw.close();
+		} catch (Exception e) {}
+    
 	}
 
 	private static void sortArray() {
 		Arrays.sort(array, new Comparator<String[]>() {
             @Override
             public int compare(String[] entry1, String[] entry2) {
-                String nr1 = entry1[1];
-                String nr2 = entry2[1];
-                return nr1.compareTo(nr2);
+                Integer nr1 = Integer.parseInt(entry1[1]);
+                Integer nr2 = Integer.parseInt(entry2[1]);
+                int sComp = nr2.compareTo(nr1);
+                
+                if (sComp != 0) {
+                    return sComp;
+                 } else {
+                    String s1 = entry1[0];
+                    String s2 = entry2[0];
+                    return s1.compareTo(s2);
+                 }
             }
         });		
 	}
@@ -82,7 +160,7 @@ public class CountNGrams {
 	}
 
 	private static void put(String word) {
-		if (sequence.length > 2) {
+		if (sequence.length > 1) {
 			for (int i = 1; i < sequence.length; i++) {
 				sequence[i-1] = sequence[i]; 
 			}
@@ -93,7 +171,9 @@ public class CountNGrams {
 	private static String sequenceToString() {
 		String seqString = "";
 		for (int i = 0; i < sequence.length; i++) {
-			seqString += sequence[i];
+			if (!sequence[i].equals("")) {
+				seqString += sequence[i] + " ";
+			}
 		}
 		return seqString;
 	}
