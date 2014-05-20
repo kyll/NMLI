33a53f5eb255ca974725b54082bb8818d3e177bf
Kyllian Broers <kyllianbroers@live.nl>
2014-05-20 12:51:00 +0200

    extra shit

--- src/BStep1.java
+++ src/BStep1.java
@@ -34,16 +34,20 @@ public class BStep1 {
 		BufferedWriter writer = new BufferedWriter(new FileWriter(output));
 		
 		Stack<String> tagStack = new Stack<String>();
-		
+		tagStack.push("");
+		Stack<String> belongStack = new Stack<String>();
+		belongStack.push("");
 		String line = reader.readLine();
 		String outLine = "";
+		
+		boolean belongFlag = false;
 		
 		// read lines untill end of file
 		while(line != null) {
 			
 			// parse line char by char
 			for (int i = 0; i < line.length(); i++) {
-				
+				System.out.println(outLine);
 				// if open bracket encountered, extract tag and add to tagStack
 				// add super tag to tag: i.e. S becomes S^ROOT
 				if(line.charAt(i) == '(') {
@@ -57,14 +61,39 @@ public class BStep1 {
 					
 					// appends tag with optional super tag
 					// TODO: shouldnt do for every tag, for correct see examples
-					if(tagStack.isEmpty()) {
-						outLine += tag + " ";
+					if(!belongFlag && line.charAt(i+1) == '(') {
+						outLine += tag + tagStack.peek() + " ";
+						tagStack.push("^" + tag);
+						belongStack.push(tagStack.peek());
+					}else if(!belongFlag && line.charAt(i+1) != '(') {
+						belongFlag = true;
+						belongStack.push(tagStack.peek());
+						String word = "";
+						i++;
+						while(line.charAt(i) != ')') {
+							word += line.charAt(i);
+							i++;
+						}
+						i++;
+						outLine += tag + " " + word + ") ";
+						tagStack.push("^" + tag);
 					}else {
-						outLine += tag + "^" + tagStack.peek() + " ";
+						tagStack.push("_" + tagStack.peek().substring(1));
+						outLine += "@" + belongStack.peek().substring(1) + "->" + tagStack.peek() + " ";
+						String word = "";
+						i++;
+						while(line.charAt(i) != ')') {
+							word += line.charAt(i);
+							i++;
+						}
+						outLine += "(" + tag + " " + word;
+						tagStack.push("^" + tag);
 					}
-					if(line.charAt(i+1) == '(') {
-						tagStack.push(tag);
-					}
+				}
+				if(line.charAt(i) == ')') {
+					outLine += ")"; 
+					tagStack.pop();
+					belongStack.pop();
 				}
 			}
 			System.out.println(outLine);
